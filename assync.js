a5033084fa10fe9af1cf549de1ad7a2ffa3b6beb
Eugene Kiprop Sang <85967099+eugenesang@users.noreply.github.com>
2021-11-19 01:28:26 +0300

    Update assync.js

--- assync.js
+++ assync.js
@@ -24,7 +24,7 @@ function divide(a,b){
         }else if(b===0){
             reject(`you are trying to divide by 0`);            
         }else{
-            resolve(`${a} divided by ${b} is ${a/b}`);
+            resolve(a/b);
         }
     })
 }
@@ -39,5 +39,60 @@ async function jumble(a,b){
     let e=await divide(c, d);
     return e;
 };
+jumble(8,9)
+.then(d=>console.log(d))
+.catch(err=>console.log(err));
 
+// without the async await it would be like this
+function jumble_(a,b){
+    divide(a, b)
+    .then(c=>{
+        divide(b,a)
+        .then(d=>{
+            divide(c, d)
+            .then(e=>{
+                return e;
+            })
+        })
+    })
+};
 
+// real life use
+// XMLhttprequest 
+/* 
+requirements
+json server
+database.json or any json file that can be served by the json server 
+json server running on port number 3000, 
+*/
+
+let req=new XMLHttpRequest();
+req.onreadystatechange=()=>{
+    if(req.readyState===4 && req.status===200){
+        let res=req.responseText;
+        res=JSON.parse(res);
+        console.log(res);
+    }
+};
+req.open("GET", "http://localhost:3000/data");
+req.send();
+
+// using the fetch api
+//it does the same thing as the clasical XMLhttprequest
+
+fetch("http://localhost:3000/data")
+.then(res=>{
+    if(res.ok){
+        return res.json();
+    }
+})
+.then(data=>{
+    console.log(data);
+})
+.catch(err=>{
+    console.log(err);
+});
+
+//the idea of fetching data from a website without interfering with the main activity is the backbone of AJAX
+//which means Asynchronous javascript and xml
+//when you start building complex web apps, this tool will be very handy
